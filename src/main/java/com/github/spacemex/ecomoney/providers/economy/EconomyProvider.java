bcceb23e9cbf6bc2a7e982e6aa67d0c1f43ecfea
deven <devenshona@gmail.com>
2024-10-09 17:31:43 -0500

    Add EconomyProvider implementation
    Introduced `EconomyProvider` to integrate with the Vault API. This class implements all required methods, using `SQLManager` for account and balance management. Bank-related methods are currently not supported and return a `NOT_IMPLEMENTED` response.

---
+++ src/main/java/com/github/spacemex/ecomoney/providers/economy/EconomyProvider.java
@@ -0,0 +1,238 @@
+package com.github.spacemex.ecomoney.providers.economy;
+
+import com.github.spacemex.ecomoney.Ecomoney;
+import com.github.spacemex.ecomoney.sql.SQLManager;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class EconomyProvider implements Economy {
+    private final FileConfiguration config;
+    private final Logger logger;
+    private final SQLManager sqlManager;
+
+    public EconomyProvider(Ecomoney plugin) {
+        this.config = plugin.getConfig();
+        this.logger = plugin.getLogger();
+        this.sqlManager = plugin.getSqlManager();
+    }
+    @Override
+    public boolean isEnabled() {
+        return config.getBoolean("setup-provider");
+    }
+
+    @Override
+    public String getName() {
+        return "EcoMoney";
+    }
+
+    @Override
+    public boolean hasBankSupport() {
+        return false;
+    }
+
+    @Override
+    public int fractionalDigits() {
+        return 2;
+    }
+
+    @Override
+    public String format(double v) {
+        return String.format("$%.2f", v);
+    }
+
+    @Override
+    public String currencyNamePlural() {
+        return "Dollar";
+    }
+
+    @Override
+    public String currencyNameSingular() {
+        return "Dollars";
+    }
+
+    @Override
+    public boolean hasAccount(String s) {
+        return sqlManager.hasAccount(s);
+    }
+
+    @Override
+    public boolean hasAccount(OfflinePlayer offlinePlayer) {
+        return hasAccount(offlinePlayer.getName());
+    }
+
+    @Override
+    public boolean hasAccount(String s, String s1) {
+        return hasAccount(s);
+    }
+
+    @Override
+    public boolean hasAccount(OfflinePlayer offlinePlayer, String s) {
+        return hasAccount(offlinePlayer.getName());
+    }
+
+    @Override
+    public double getBalance(String s) {
+        return sqlManager.getBalance(s);
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer offlinePlayer) {
+        return getBalance(offlinePlayer.getName());
+    }
+
+    @Override
+    public double getBalance(String s, String s1) {
+        return getBalance(s);
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer offlinePlayer, String s) {
+       return getBalance(offlinePlayer.getName());
+    }
+
+    @Override
+    public boolean has(String s, double v) {
+        return sqlManager.hasBalance(s,v);
+    }
+
+    @Override
+    public boolean has(OfflinePlayer offlinePlayer, double v) {
+        return has(offlinePlayer.getName(),v);
+    }
+
+    @Override
+    public boolean has(String s, String s1, double v) {
+        return has(s,v);
+    }
+
+    @Override
+    public boolean has(OfflinePlayer offlinePlayer, String s, double v) {
+        return has(offlinePlayer.getName(),v);
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(String s, double v) {
+            return sqlManager.withdrawFunds(s,v);
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(OfflinePlayer offlinePlayer, double v) {
+        return withdrawPlayer(offlinePlayer.getName(),v);
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(String s, String s1, double v) {
+        return withdrawPlayer(s,v);
+    }
+
+    @Override
+    public EconomyResponse withdrawPlayer(OfflinePlayer offlinePlayer, String s, double v) {
+        return withdrawPlayer(offlinePlayer.getName(),v);
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(String s, double v) {
+        return sqlManager.depositFunds(s,v);
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(OfflinePlayer offlinePlayer, double v) {
+        return depositPlayer(offlinePlayer.getName(),v);
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(String s, String s1, double v) {
+        return depositPlayer(s,v);
+    }
+
+    @Override
+    public EconomyResponse depositPlayer(OfflinePlayer offlinePlayer, String s, double v) {
+        return depositPlayer(offlinePlayer.getName(),v);
+    }
+
+    @Override
+    public EconomyResponse createBank(String s, String s1) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse createBank(String s, OfflinePlayer offlinePlayer) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse deleteBank(String s) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse bankBalance(String s) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse bankHas(String s, double v) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse bankWithdraw(String s, double v) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse bankDeposit(String s, double v) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse isBankOwner(String s, String s1) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse isBankOwner(String s, OfflinePlayer offlinePlayer) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse isBankMember(String s, String s1) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public EconomyResponse isBankMember(String s, OfflinePlayer offlinePlayer) {
+        return new EconomyResponse(0,0, EconomyResponse.ResponseType.NOT_IMPLEMENTED,null);
+    }
+
+    @Override
+    public List<String> getBanks() {
+        return List.of();
+    }
+
+    @Override
+    public boolean createPlayerAccount(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean createPlayerAccount(OfflinePlayer offlinePlayer) {
+        return sqlManager.createPlayerAccount(offlinePlayer);
+    }
+
+    @Override
+    public boolean createPlayerAccount(String s, String s1) {
+        return false;
+    }
+
+    @Override
+    public boolean createPlayerAccount(OfflinePlayer offlinePlayer, String s) {
+        return false;
+    }
+}
